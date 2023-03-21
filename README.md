4a1a253bfbc2392b05bd24b538b4ea395ab038db
leninner <mazabandalenin180@gmail.com>
2023-03-21 08:37:36 -0500

    add discord interfaces to send the correct content and add moddy middleware to reduce the workload

--- README.md
+++ README.md
@@ -125,3 +125,5 @@ aws cloudformation delete-stack --stack-name matchmaker
 See the [AWS SAM developer guide](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/what-is-sam.html) for an introduction to SAM specification, the SAM CLI, and serverless application concepts.
 
 Next, you can use AWS Serverless Application Repository to deploy ready to use Apps that go beyond hello world samples and learn how authors developed their applications: [AWS Serverless Application Repository main page](https://aws.amazon.com/serverless/serverlessrepo/)
+
+- [Discord Colors](https://gist.github.com/thomasbnt/b6f455e2c7d743b796917fa3c205f812)
--- src/handlers/send-pair-notifier.ts
+++ src/handlers/send-pair-notifier.ts
@@ -1,34 +1,27 @@
+import { injectLambdaContext } from '@aws-lambda-powertools/logger';
+import middy from '@middy/core';
 import { SQSEvent } from 'aws-lambda';
 import { logger } from '../common';
 import { Config } from '../config';
 import { DiscordClient } from '../gateway/DiscordClient';
+import { ExecuteWebhookParams } from '../gateway/interfaces/IDiscord';
+import sqsJsonBodyParser from '@middy/sqs-json-body-parser';
 
-export const lambdaHandler = async (event: SQSEvent): Promise<void> => {
+const lambdaHandler = async (event: SQSEvent): Promise<void> => {
     logger.info('[SQS received] Lambda invoked', {
         details: { event },
     });
     const { Records } = event;
+    const params = Records[0].body as unknown as ExecuteWebhookParams;
     const { id, token } = Config.getDiscordWebhookCredentials();
-
-    logger.info('[SQS received] Processing records', {
-        details: { id, token },
-    });
 
     const webhook = new DiscordClient({ id, token });
 
     try {
-        await webhook.send({
-            embeds: [
-                {
-                    title: 'Mensaje de prueba',
-                    description: 'Mensaje de prueba',
-                    color: 16711680,
-                },
-            ],
-        });
+        await webhook.send(params);
 
-        logger.info('[SQS received] Processing records', {
-            details: { Records },
+        logger.info('[DISCORD] Sended message', {
+            details: { params },
         });
     } catch (e) {
         logger.info('[SQS received] Error sending message to discord', {
@@ -36,3 +29,5 @@ export const lambdaHandler = async (event: SQSEvent): Promise<void> => {
         });
     }
 };
+
+export const handler = middy(lambdaHandler).use(injectLambdaContext(logger)).use(sqsJsonBodyParser());
