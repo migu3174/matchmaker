acddc17d0056bdf02e85c270890a8546db6d4480
leninner <mazabandalenin180@gmail.com>
2023-03-21 10:21:02 -0500

    Update params to send to discord

--- src/handlers/send-pair-notifier.ts
+++ src/handlers/send-pair-notifier.ts
@@ -7,12 +7,27 @@ import { DiscordClient } from '../gateway/DiscordClient';
 import { ExecuteWebhookParams } from '../gateway/interfaces/IDiscord';
 import sqsJsonBodyParser from '@middy/sqs-json-body-parser';
 
+const GREEN_COLOR = 5763719;
+const TITLE = 'Pairs para la presente semana';
+
 const lambdaHandler = async (event: SQSEvent): Promise<void> => {
     logger.info('[SQS received] Lambda invoked', {
         details: { event },
     });
     const { Records } = event;
-    const params = Records[0].body as unknown as ExecuteWebhookParams;
+    const pairs = Records[0].body as unknown as any[];
+
+    const params: ExecuteWebhookParams = {
+        embeds: [
+            {
+                title: TITLE,
+                description: pairs.reduce((acc, pair) => {
+                    return `${acc} - ${pair}\n`;
+                }, ''),
+                color: GREEN_COLOR,
+            },
+        ],
+    };
     const { id, token } = Config.getDiscordWebhookCredentials();
 
     const webhook = new DiscordClient({ id, token });
